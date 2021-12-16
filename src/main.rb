24590d30984479ed266808817ff9fa75fb526216
Nga Rampling <ngarampling@Ngas-MacBook-Air.local>
2021-12-17 00:35:07 +1100

    created options to save_calc_budget or to input_budget again

--- src/main.rb
+++ src/main.rb
@@ -3,22 +3,47 @@ require_relative './classes/input_budget.rb'
 
 #Initialising Questions and Input
 puts "What is your name?"
-name = gets.chomp
+user_name = gets.chomp
 
-puts "Hi #{name}, what is your expected income this week?"
-income = gets.chomp.to_i
-puts "What is your total expenses this week?"
-expenses = gets.chomp.to_i
-puts "Enter the amount of savings you'd like to achieve in this week's budget"
-savings = gets.chomp.to_i
+def input_budget(name)
+    puts "Hi #{name}, what is your expected income this week?"
+    income = gets.chomp.to_i
+    puts "What is your total expenses this week?"
+    expenses = gets.chomp.to_i
+    puts "Enter the amount of savings you'd like to achieve in this week's budget"
+    savings = gets.chomp.to_i
+    return income, expenses, savings
+end
+
+def save_calc_budget(income, expenses, savings)
+    puts "This is to save and calculate method"
+end
+
+weekly_budget = input_budget(user_name)
 
 #2nd feature - Listing all entries and asking user to verify.
 #Present users with options to select.
 
-puts "Please confirm the following information entered:".red
-puts "This week's income is $#{income}"
-puts "This week's total expenses is $#{expenses}"
-puts "You'd like to add $#{savings} to your savings account"
+puts "Please review the information you have entered:".green
+puts "This week's income is $#{weekly_budget[0]}."
+puts "This week's total expenses is $#{weekly_budget[1]}."
+puts "You'd like to add $#{weekly_budget[2]} to your savings account."
+
+puts "Enter 1 to save & calculate budget"
+puts "Enter 2 to start over"
+
+input = gets.chomp.to_i
+if input == 1
+    save_calc_budget(weekly_budget[0], weekly_budget[1], weekly_budget[2])
+elsif input == 2
+    weekly_budget = input_budget(user_name)
+    
+else
+    puts "Please enter the number 1 or 2 only"
+end
+
+
+
 
 
 
